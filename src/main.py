e4a6006cab9ebf09422631cb0086f427a7d8404a
キャンド <ann.mauduy.decius@gmail.com>
2023-07-09 18:34:13 -0400

    change port

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from lib.client import CuteBerry
 
 HOST = "127.0.0.1"
-PORT = 7878  
+PORT = 8080  
 USERID = 331431342438875137
         
 
 def main():
     cute_berry = CuteBerry(HOST, PORT)
 
-    print(cute_berry.get_recipe(90000))
+    print(cute_berry.get_user(331431342438875137))
